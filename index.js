90ba37844e53a9e73a2be1997dce2f7f2ed75ad3
Khanisic <62929300+Khanisic@users.noreply.github.com>
2020-06-18 06:35:18 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,77 @@
            // Responsive Nav
 
-$(document).ready(function() {
-           $(".menu-icon").on("click", function() {
-                 $("nav ul").toggleClass("showing");
+           $(document).ready(function() {
+             $(".menu-icon").on("click", function() {
+               $("nav ul").toggleClass("showing");
+             });
            });
-     });
 
-     // Scrolling Effect
+           // Scrolling Effect
 
-     $(window).on("scroll", function() {
-           if($(window).scrollTop()) {
-                 $('nav').addClass('black');
+           $(window).on("scroll", function() {
+             if ($(window).scrollTop()) {
+               $('nav').addClass('black');
+             } else {
+               $('nav').removeClass('black');
+             }
+           })
+
+           // Statistics Scrolling Animation
+
+
+           $(window).scroll(function() {
+             var scroll = $(window).scrollTop();
+             if (scroll > 1000) {
+               $(".acts").addClass("animate__backInUp");
+             }
+           })
+
+           $(window).scroll(function() {
+             var scroll = $(window).scrollTop();
+             if (scroll > 1500) {
+               $(".col-sm-6").addClass("animate__backInLeft");
+             }
+           })
+
+
+           $(window).scroll(function() {
+             var scroll = $(window).scrollTop();
+             if (scroll > 1500) {
+               $(".location").addClass("animate__backInRight");
+             }
+           })
+
+
+           $(window).scroll(function() {
+             var scroll = $(window).scrollTop();
+             if (scroll > 1550) {
+               $(".fa-instagram").addClass("animate__backInUp");
+             }
+           })
+
+           $(window).scroll(function() {
+             var scroll = $(window).scrollTop();
+             if (scroll > 1550) {
+               $(".fa-whatsapp").addClass("animate__backInDown");
+             }
+           })
+
+           $(window).scroll(function() {
+             var scroll = $(window).scrollTop();
+             if (scroll > 1550) {
+               $(".fa-facebook").addClass("animate__backInUp");
+             }
+           })
+
+           var prevScrollpos = window.pageYOffset;
+           window.onscroll = function() {
+             var currentScrollPos = window.pageYOffset;
+             if (prevScrollpos > currentScrollPos) {
+               document.getElementById("nav").style.top = "0px";
+               document.getElementById("nav").style.transition = "0.3s";
+             } else {
+               document.getElementById("nav").style.top = "-77px";
+               document.getElementById("nav").style.transition = "0.3s";
+             }
+             prevScrollpos = currentScrollPos;
            }
-
-           else {
-                 $('nav').removeClass('black');
-           }
-     })
-
-     // Statistics Scrolling Animation
-
-
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
-         if (scroll > 1200) {
-             $(".acts").addClass("animate__backInUp");
-         }
-     })
-
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
-         if (scroll > 1500) {
-             $(".col-sm-6").addClass("animate__backInLeft");
-         }
-     })
-
-
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
-         if (scroll > 1500) {
-             $(".location").addClass("animate__backInRight");
-         }
-     })
-
-
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
-         if (scroll > 1550) {
-             $(".fa-instagram").addClass("animate__backInUp");
-         }
-     })
-
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
-         if (scroll > 1550) {
-             $(".fa-whatsapp").addClass("animate__backInDown");
-         }
-     })
-
-     $(window).scroll(function() {
-         var scroll = $(window).scrollTop();
-         if (scroll > 1550) {
-             $(".fa-facebook").addClass("animate__backInUp");
-         }
-     })
